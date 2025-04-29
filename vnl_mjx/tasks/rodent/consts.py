23ace226b953abcb874abfce7c00e6726def9162
scott-yj-yang <scottyang177@gmail.com>
2025-04-29 00:29:51 +0000

    add box feet

--- vnl_mjx/tasks/rodent/consts.py
+++ vnl_mjx/tasks/rodent/consts.py
@@ -9,4 +9,5 @@ RODENT_PATH = epath.Path(__file__).parent
 
 RODENT_XML_PATH = RODENT_PATH / "xmls" / "rodent.xml"
 RODENT_SPHERE_FEET_PATH = RODENT_PATH / "xmls" / "rodent_sphere_feet.xml"
+RODENT_BOX_FEET_PATH = RODENT_PATH / "xmls" / "rodent_box_feet.xml"
 ARENA_XML_PATH = RODENT_PATH / "xmls" / "arena.xml"
