7e9e204d22fbaf6d7651901c3a9f138355bd047e
Eric Leonardis <leonardiseric@gmail.com>
2025-05-01 17:59:53 +0000

    revert changes to main

--- vnl_mjx/tasks/mouse/consts.py
+++
@@ -1,9 +0,0 @@
-"""Defines rodents constants."""
-
-from etils import epath
-
-from mujoco_playground._src import mjx_env
-
-MOUSE_PATH = epath.Path(__file__).parent
-
-MOUSE_XML_PATH = MOUSE_PATH / "xmls" / "akira_torque.xml"
