a446ea858947ddc3b9fa60c22430b1f7e2ac26e2
Eric Leonardis <leonardiseric@gmail.com>
2025-05-20 02:49:11 +0000

    load muscle model

--- vnl_mjx/tasks/mouse/consts.py
+++ vnl_mjx/tasks/mouse/consts.py
@@ -6,4 +6,4 @@ from mujoco_playground._src import mjx_env
 
 MOUSE_PATH = epath.Path(__file__).parent
 
-MOUSE_XML_PATH = MOUSE_PATH / "xmls" / "akira_torque.xml"
+MOUSE_XML_PATH = MOUSE_PATH / "xmls" / "akira_muscle.xml"
