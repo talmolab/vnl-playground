80420ece4713b1f9bcafd429fde782ef27a32aae
scott-yj-yang <scottyang177@gmail.com>
2026-01-10 04:44:04 +0000

    added full collision to the consts

--- vnl_playground/tasks/rodent/consts.py
+++ vnl_playground/tasks/rodent/consts.py
@@ -8,6 +8,7 @@ from mujoco_playground._src import mjx_env
 RODENT_PATH = epath.Path(__file__).parent
 
 RODENT_XML_PATH = RODENT_PATH / "xmls" / "rodent.xml"
+RODENT_FULL_COLLISION_XML = RODENT_PATH / "xmls" / "rodent_full_collisions.xml"
 RODENT_BOX_FEET_PATH = RODENT_PATH / "xmls" / "rodent_box_feet.xml"
 ARENA_XML_PATH = RODENT_PATH / "xmls" / "arena.xml"
 WHITE_ARENA_XML_PATH = RODENT_PATH / "xmls" / "white_arena.xml"
