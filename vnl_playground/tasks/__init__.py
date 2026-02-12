98d85ce019cc3aab8b75374ef13e850455497837
Eric Leonardis <leonardiseric@gmail.com>
2026-02-12 05:57:43 +0000

    register mouse reach and imitation

--- vnl_playground/tasks/__init__.py
+++ vnl_playground/tasks/__init__.py
@@ -15,6 +15,9 @@ from vnl_playground.tasks.rodent import bowl_escape as rodent_bowl_escape
 from vnl_playground.tasks.rodent import maintain_velocity as rodent_maintain_velocity
 from vnl_playground.tasks.rodent import joystick as rodent_joystick
 from vnl_playground.tasks.fruitfly import imitation as fruitfly_imitation
+from vnl_playground.tasks.mouse import imitation as mouse_imitation
+from vnl_playground.tasks.mouse import mouse_reach
+from vnl_playground.tasks.mouse.reference_clips import MouseReferenceClips
 
 # Unified wrappers and reference clips
 from vnl_playground.tasks.wrappers import FlattenObsWrapper
@@ -29,6 +32,8 @@ _envs = {
     "RodentMaintainVelocity": rodent_maintain_velocity.MaintainVelocity,
     "RodentJoystick": rodent_joystick.Joystick,
     "FruitflyImitation": fruitfly_imitation.Imitation,
+    "MouseReach": mouse_reach.MouseReach,
+    "MouseImitation": mouse_imitation.MouseImitation,
 }
 
 _cfgs = {
@@ -39,6 +44,8 @@ _cfgs = {
     "RodentMaintainVelocity": rodent_maintain_velocity.default_config,
     "RodentJoystick": rodent_joystick.default_config,
     "FruitflyImitation": fruitfly_imitation.default_config,
+    "MouseReach": mouse_reach.default_config,
+    "MouseImitation": mouse_imitation.default_config,
 }
 
 # ReferenceClips class for imitation environments (not all envs use clips)
@@ -46,6 +53,7 @@ _reference_clips_classes = {
     "RodentImitation": ReferenceClips,
     "RodentSparseImitation": ReferenceClips,
     "FruitflyImitation": ReferenceClips,
+    "MouseImitation": MouseReferenceClips,
 }
 
 
