95eea5bf480f5de0068c4a1bc583d8bab6e3ec1b
charles-zhng <charles.zhang216@gmail.com>
2026-02-12 21:20:36 -0500

    Remove unused fruitfly and stick __init__.py files
    All imports use direct submodule paths, so these re-export
    files are unnecessary.

--- vnl_playground/tasks/fruitfly/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Fruitfly task environments."""
-
-from vnl_playground.tasks.fruitfly.base import FruitflyEnv
-from vnl_playground.tasks.fruitfly.imitation import Imitation
-from vnl_playground.tasks.fruitfly.maintain_velocity import MaintainVelocity
-from vnl_playground.tasks.fruitfly import consts
-
-__all__ = ["FruitflyEnv", "Imitation", "MaintainVelocity", "consts"]
