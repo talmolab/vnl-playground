f1af5dfa113cc022c8b607ed9c3cc978c07b81cf
aaprasad <aaprasad@mit.edu>
2026-05-13 10:22:46 -0400

    update celegans xml to capsule model

--- vnl_playground/tasks/celegans/consts.py
+++ vnl_playground/tasks/celegans/consts.py
@@ -10,8 +10,8 @@ from etils import epath
 
 CELEGANS_PATH: epath.Path = epath.Path(__file__).parent
 
-CELEGANS_XML_PATH: epath.Path = CELEGANS_PATH / "xmls" / "celegans_cgs.xml"
-ARENA_XML_PATH: epath.Path = CELEGANS_PATH / "xmls" / "arena_cgs.xml"
+CELEGANS_XML_PATH: epath.Path = CELEGANS_PATH / "xmls" / "celegans.xml"
+ARENA_XML_PATH: epath.Path = CELEGANS_PATH / "xmls" / "arena.xml"
 WHITE_ARENA_XML_PATH: epath.Path = CELEGANS_PATH / "xmls" / "white_arena.xml"
 REFERENCE_H5_PATH: epath.Path = CELEGANS_PATH / "reference_data" / "reference2d.h5"
 
